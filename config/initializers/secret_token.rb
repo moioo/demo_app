c2790991f869449348830b87c938b3f5327ba74f
moioo <moioo@qq.com>
2011-08-14 00:52:06 +0800

    moioo frist ruby app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = 'a9902fa63900bb1322a155a71cc5c2d10423e4b3ed679c0ac4fe9c015ac6619b0661e825155fddcd2295e2b13b2cfc5eb7f715ea799f5e1d2238c625cae4b6ea'
